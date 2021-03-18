56a55f18788cb5b59bb993b4d35910d4bff7f56e
Jochem Vogel <voge0031@hz.nl>
2021-03-18 17:37:17 +0100

    Add working form
    Does not store anything, but console log result in terminal after submit

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,4 +2,20 @@ function getAdmin(req, res) {
 	res.render('admin/index');
 }
 
-module.exports = { getAdmin };
+function getEditMatch(req, res) {
+	res.render('admin/edit-match');
+}
+
+function postEditMatch(req, res) {
+	const team1 = req.body.team1;
+	const team2 = req.body.team2;
+	const time = req.body.time;
+	const scoreTeam1 = req.body.score1;
+	const scoreTeam2 = req.body.score2;
+
+	console.log(req.body);
+
+	res.render('admin/index');
+}
+
+module.exports = { getAdmin, postEditMatch, getEditMatch };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,9 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/', adminController.getAdmin);
+router
+	.get('/', adminController.getAdmin)
+	.get('/edit-match', adminController.getEditMatch)
+	.post('/edit-match', adminController.postEditMatch);
 
 module.exports = router;
