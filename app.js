c3d45d5ba98cb3a79c2d9cb3622599d63db3fff2
Jochem Vogel <voge0031@hz.nl>
2021-03-26 17:25:11 +0100

    Update port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT | 4000;
 
 const adminRoutes = require('./routes/admin');
 const matchesRoutes = require('./routes/matches');
