7aef02e39fe5118b71ba667daf3ac42904b7dedc
Jochem Vogel <voge0031@hz.nl>
2021-03-26 17:05:15 +0100

    Remove socket script and add npm start command

--- socket.js
+++ socket.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
-const PORT = 8001;
+// const PORT = 8001;
 
-// const notification = require('./notifications')
+// // const notification = require('./notifications')
 
-io.on('connection', socket => {
-    socket.on('matchUpdate', ({title, body}) => {
-        console.log('test');
-        // notification.newNotification(title, body);
-    })
-})
+// io.on('connection', socket => {
+//     socket.on('matchUpdate', ({title, body}) => {
+//         console.log('test');
+//         // notification.newNotification(title, body);
+//     })
+// })
 
-app.listen(PORT, () => console.log(`Socket listening on port ${8001}`));
+// app.listen(PORT, () => console.log(`Socket listening on port ${8001}`));
